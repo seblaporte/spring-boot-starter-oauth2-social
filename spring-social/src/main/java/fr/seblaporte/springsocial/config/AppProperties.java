8fe1c4394a0e3353f9aea548d9a402a9284930b5
sebastien.laporte <seb_laporte_33@yahoo.com>
2019-02-10 09:55:17 +0100

    Suppression méthode non utilisée

--- spring-social/src/main/java/fr/seblaporte/springsocial/config/AppProperties.java
+++ spring-social/src/main/java/fr/seblaporte/springsocial/config/AppProperties.java
@@ -7,10 +7,12 @@ import java.util.List;
 
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
+
     private final Auth auth = new Auth();
     private final OAuth2 oauth2 = new OAuth2();
 
     public static class Auth {
+
         private String tokenSecret;
         private long tokenExpirationMsec;
 
@@ -32,15 +34,11 @@ public class AppProperties {
     }
 
     public static final class OAuth2 {
+
         private List<String> authorizedRedirectUris = new ArrayList<>();
 
         public List<String> getAuthorizedRedirectUris() {
             return authorizedRedirectUris;
-        }
-
-        public OAuth2 authorizedRedirectUris(List<String> authorizedRedirectUris) {
-            this.authorizedRedirectUris = authorizedRedirectUris;
-            return this;
         }
     }
 
